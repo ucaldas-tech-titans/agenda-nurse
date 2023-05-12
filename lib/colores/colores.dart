1b650157d05d07f1646a537eb3c1b5ac9236f2d4
Juan José González Giraldo <juan.1701716389@ucaldas.edu.co>
2023-05-12 08:44:59 -0500

    Improve editing shifts UX for mobile

--- lib/colores/colores.dart
+++ lib/colores/colores.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 final colores = {
   // "verde": Color.fromRGBO(28, 166, 138, 1),
-  "verde": Color.fromARGB(255, 43, 200, 169),
-  "azul": Color.fromRGBO(34, 136, 200, 1),
-  "blanco":Color.fromARGB(255, 255, 255, 255),
-  "gris":Color.fromARGB(244, 245, 248, 255),
-  "negro":Color.fromARGB(255, 0, 0, 0),
+  "verde": const Color.fromARGB(255, 43, 200, 169),
+  "azul": const Color.fromRGBO(34, 136, 200, 1),
+  "blanco":const Color.fromARGB(255, 255, 255, 255),
+  "gris":const Color.fromARGB(244, 245, 248, 255),
+  "negro":const Color.fromARGB(255, 0, 0, 0),
 };
