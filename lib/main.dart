1fec19468db2f312f1354956e972d6e685309fe5
Felipe giraldo <shiomediaz@gmail.com>
2023-06-08 01:10:43 -0500

    Creacion Vista y boton create_nurse, ademas d eañadir el logo en la vista main.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:agendanurse/widgets/change_nurse_shift.dart';
+import 'package:agendanurse/widgets/create_nurse.dart';
 import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
@@ -29,21 +30,44 @@ class MyApp extends StatelessWidget {
 }
 
 class HomeView extends StatelessWidget {
-  const HomeView({super.key});
+  const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Nurse Shift Manager')),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const ChangeNurseShift()),
-            );
-          },
-          child: const Text('Manage Nurse Shifts'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'lib/assets/icons/ico_nurse.png', // Ruta de tu imagen de logo
+              width: 100, // Ajusta el ancho de acuerdo a tus necesidades
+              height: 100, // Ajusta la altura de acuerdo a tus necesidades
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const ChangeNurseShift()),
+                );
+              },
+              child: const Text('Manage Nurse Shifts'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const CreateNurseView()),
+                );
+              },
+              child: const Text('Create Nurse'),
+            ),
+          ],
         ),
       ),
     );
